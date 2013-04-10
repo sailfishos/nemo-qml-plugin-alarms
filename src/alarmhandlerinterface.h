21bc458129da3501618b062e13dcf828956509d8
Petri M. Gerdt <petri.gerdt@jollamobile.com>
2013-04-10 15:08:51 +0300

    [alarms] Wrap voland signal adaptor to prevent overexposing

--- src/alarmhandlerinterface.h
+++ src/alarmhandlerinterface.h
@@ -47,6 +47,7 @@
 
 class VolandAdaptor;
 class VolandSignalAdaptor;
+class VolandSignalWrapper;
 class AlarmDialogObject;
 
 namespace Maemo {
@@ -81,7 +82,6 @@ public:
      *  that the display may be turned off.
      */
     Q_PROPERTY(bool dialogOnScreen READ dialogOnScreen WRITE setDialogOnScreen NOTIFY dialogOnScreenChanged)
-
     bool dialogOnScreen();
     void setDialogOnScreen(bool onScreen);
 
@@ -111,7 +111,7 @@ private slots:
 
 private:
     VolandAdaptor *adaptor;
-    VolandSignalAdaptor *signalAdaptor;
+    VolandSignalWrapper *signalWrapper;
     QHash<int, AlarmDialogObject*> dialogs;
     bool m_dialogOnScreen;
 
@@ -138,6 +138,19 @@ public:
     virtual bool open(const Maemo::Timed::Voland::Reminder &data);
     virtual bool open(const QList<QVariant> &data);
     virtual bool close(uint cookie);
+};
+
+class VolandSignalWrapper: public QObject
+{
+    Q_OBJECT
+
+public:
+    VolandSignalWrapper(QObject *parent);
+    void setupInterface();
+
+signals:
+    void visual_reminders_status(int status);
+    void error(const QString &message);
 };
 
 class VolandSignalAdaptor:  public QDBusAbstractAdaptor
